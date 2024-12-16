e21753cbc719b50d731d475ce1605eda7e9bd062
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-16 12:56:16 +0700

    menambahkan fungsi pada LocalRepositoryMhs untuk menghubungkan MahasiswaDao dengan operasi di RepositoryMhs

--- app/src/main/java/com/example/project7/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/project7/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,24 @@ package com.example.project7.repository
 
 import com.example.project7.data.dao.MahasiswaDao
 import com.example.project7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ): RepositoryMhs {    //implementasi dari RepositoryMhs
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
     }
 }
