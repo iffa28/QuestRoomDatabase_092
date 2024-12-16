a4a775b513fd12fca4a976878c954df6bedd82bf
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-16 13:37:17 +0700

    implementasi route agar destinasi nya hanya ke home

---
+++ app/src/main/java/com/example/project7/ui/view/mahasiswa/HomeMhsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.project7.ui.view.mahasiswa
+
+import com.example.project7.ui.navigation.AlamatNavigasi
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
