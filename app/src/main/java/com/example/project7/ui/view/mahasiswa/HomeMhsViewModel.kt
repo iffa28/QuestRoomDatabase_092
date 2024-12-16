5ae73fd66d02aa934e87e017767beecf4939fad1
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-16 16:50:52 +0700

    membuat card lalu memanggil entitas mahasiswa

--- app/src/main/java/com/example/project7/ui/view/mahasiswa/HomeMhsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.project7.ui.view.mahasiswa
-
-import com.example.project7.ui.navigation.AlamatNavigasi
-
-object DestinasiHome : AlamatNavigasi {
-    override val route = "home"
-}
