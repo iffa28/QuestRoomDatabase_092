f09141d1181acfe4ea562ff37c9f373d9788244f
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-16 15:05:06 +0700

    menambahkan class HomeMhsViewModel dengan repositoryMhs untuk mendapatkan data

--- app/src/main/java/com/example/project7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/project7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,7 +1,50 @@
 package com.example.project7.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.project7.data.entity.Mahasiswa
+import com.example.project7.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUIState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
