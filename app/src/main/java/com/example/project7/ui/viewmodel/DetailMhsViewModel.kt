8b1fe59f7df388433b60617ad4cad3c92b7418a6
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-16 15:28:19 +0700

    menambahkan class DetailMhsViewModel, lalu mengambil data nim dari navigasi menggunakan SavedStateHandle

--- app/src/main/java/com/example/project7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/project7/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,6 +1,30 @@
 package com.example.project7.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.project7.data.entity.Mahasiswa
+import com.example.project7.repository.RepositoryMhs
+import com.example.project7.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+
+
+}
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
