bf1f433d9aa092db858facb43775556714494909
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-16 12:32:21 +0700

    menambahkan fungsi untuk mengambil semua data mahasiswa berdasrkan urutan kecil ke besar

--- app/src/main/java/com/example/project7/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/project7/data/dao/MahasiswaDao.kt
@@ -1,13 +1,19 @@
 package com.example.project7.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.project7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
-    suspend fun insertMahasiswa(
-        mahasiswa: Mahasiswa
-    )
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
 }
