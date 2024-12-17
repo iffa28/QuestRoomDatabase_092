1bac9f8c1d3ab55b961a4a523acade1f05b80a92
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-17 15:00:15 +0700

    menambahkan fungsi BodyDetailMhs, variabel konfirmasi delete dan menampilkan loading

--- app/src/main/java/com/example/project7/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/project7/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,22 +1,49 @@
 package com.example.project7.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.project7.data.entity.Mahasiswa
+import com.example.project7.ui.viewmodel.DetailUiState
+
+@Composable
+fun BodyDetailMhs(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() //tampilkan loading
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailMhs(
