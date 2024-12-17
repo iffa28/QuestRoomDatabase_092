51dfa01f47674b753c73fbb9c952d7273fbad764
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-17 16:17:34 +0700

    menambahkan scaffold untuk layout Edit Mahasiswa

--- app/src/main/java/com/example/project7/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/project7/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,17 +1,27 @@
 package com.example.project7.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Update
+import com.example.project7.ui.customwidget.TopAppBar
 import com.example.project7.ui.viewmodel.PenyediaViewModel
 import com.example.project7.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun UpdateMhs(
@@ -38,5 +48,46 @@ fun UpdateMhs(
                 viewModel.resetSnackBarMessage()
             }
         }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }, //tempatkan snackbar di scaffold
+        topBar = {
+            TopAppBar(
+                judul = "Edit Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+
+        ) {
+            //Isi Body
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate()
+                            }
+                        }
+                    }
+                }
+            )
+
+        }
+
     }
 }
