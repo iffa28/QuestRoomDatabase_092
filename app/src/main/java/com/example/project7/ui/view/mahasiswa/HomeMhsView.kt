b597858211364dc04fa4ef649010ec8059e29fdd
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-17 13:02:46 +0700

    fungsi membuat BodyHomeMhsView

--- app/src/main/java/com/example/project7/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/project7/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,9 +1,11 @@
 package com.example.project7.ui.view.mahasiswa
 
 import androidx.annotation.OptIn
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,16 +15,32 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.project7.data.entity.Mahasiswa
+import com.example.project7.ui.viewmodel.HomeUiState
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+}
 
 @Composable
 fun ListMahasiswa(
